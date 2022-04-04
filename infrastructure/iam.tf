69a58354a2c01b077d038c537ac75568d3fad4f6
davidcava <david.cava.pina@gmail.com>
2022-04-04 21:20:29 +0100

    add iam permissions for pubsub

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -26,3 +26,21 @@ resource "google_cloudfunctions_function_iam_binding" "insta_downloader_invoker_
     "serviceAccount:${google_service_account.cloud_function_invoker_account.email}",
   ]
 }
+
+resource "google_pubsub_topic_iam_binding" "insta_download_publisher_binding" {
+  project = google_pubsub_topic.insta_download_jobs.project
+  topic   = google_pubsub_topic.insta_download_jobs.name
+  role    = "roles/pubsub.publisher"
+  members = [
+    "serviceAccount:${google_service_account.cloud_function_invoker_account.email}",
+  ]
+}
+
+resource "google_pubsub_topic_iam_binding" "insta_download_subscriber_binding" {
+  project = google_pubsub_topic.insta_download_jobs.project
+  topic   = google_pubsub_topic.insta_download_jobs.name
+  role    = "roles/pubsub.subscriber"
+  members = [
+    "serviceAccount:${google_service_account.cloud_function_invoker_account.email}",
+  ]
+}
