f5c24b89886c9e44e7b1156d31c467bf2c5f877c
davidcava <david.cava.pina@gmail.com>
2022-04-04 15:05:54 +0100

    alluser invoker of pusher cf

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -6,3 +6,13 @@ resource "google_pubsub_topic_iam_binding" "insta_jobs_publisher" {
     "serviceAccount:${google_service_account.cloud_function_invoker_account.email}",
   ]
 }
+
+resource "google_cloudfunctions_function_iam_binding" "pusher_invoker_binding" {
+  project        = google_cloudfunctions_function.pusher.project
+  region         = google_cloudfunctions_function.pusher.region
+  cloud_function = google_cloudfunctions_function.pusher.name
+  role           = "roles/invoker"
+  members = [
+    "allUsers",
+  ]
+}
