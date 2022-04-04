18bb3e2109204c7fd78d58386327cd46fc0d351d
davidcava <david.cava.pina@gmail.com>
2022-04-04 12:04:54 +0100

    remove insta loader func for now

--- infrastructure/functions.tf
+++ infrastructure/functions.tf
@@ -1,35 +1,35 @@
 # Function: Execute Insta Downloads
 #
-data "local_file" "insta_downloader" {
-  filename = "functions/insta_downloader.zip"
-}
+# data "local_file" "insta_downloader" {
+#   filename = "functions/insta_downloader.zip"
+# }
 
-resource "google_storage_bucket_object" "insta_downloader" {
-  name   = format("insta_downloader.zip#%s", md5(data.local_file.insta_downloader.content))
-  bucket = google_storage_bucket.deployer.name
-  source = data.local_file.insta_downloader.filename
-}
+# resource "google_storage_bucket_object" "insta_downloader" {
+#   name   = format("insta_downloader.zip#%s", md5(data.local_file.insta_downloader.content))
+#   bucket = google_storage_bucket.deployer.name
+#   source = data.local_file.insta_downloader.filename
+# }
 
-resource "google_cloudfunctions_function" "insta_downloader" {
-  name         = "insta_downloader"
-  runtime      = "python38"
-  entry_point  = "insta_downloader"
-  trigger_http = true
-  timeout      = 540
-  region       = local.gcs_region
+# resource "google_cloudfunctions_function" "insta_downloader" {
+#   name         = "insta_downloader"
+#   runtime      = "python38"
+#   entry_point  = "insta_downloader"
+#   trigger_http = true
+#   timeout      = 540
+#   region       = local.gcs_region
 
-  source_archive_bucket = google_storage_bucket.deployer.name
-  source_archive_object = google_storage_bucket_object.insta_downloader.name
+#   source_archive_bucket = google_storage_bucket.deployer.name
+#   source_archive_object = google_storage_bucket_object.insta_downloader.name
 
-  environment_variables = {
-    GCP_REGION   = local.gcs_region
-    BUCKET_NAME  = google_storage_bucket.raw_videos.name
-    SLACK_URL    = var.slack_insta_url
-    ENVIRONMENT  = var.workspace
-    SLACK_SECRET = var.slack_secret
-  }
-  depends_on = [google_storage_bucket.raw_videos, google_service_account.cloud_function_invoker_account]
-}
+#   environment_variables = {
+#     GCP_REGION   = local.gcs_region
+#     BUCKET_NAME  = google_storage_bucket.raw_videos.name
+#     SLACK_URL    = var.slack_insta_url
+#     ENVIRONMENT  = var.workspace
+#     SLACK_SECRET = var.slack_secret
+#   }
+#   depends_on = [google_storage_bucket.raw_videos, google_service_account.cloud_function_invoker_account]
+# }
 
 # Function: Push task to PubSub
 #
