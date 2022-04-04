d743271246386d61957e855b6dcfaa1efa231030
davidcava <david.cava.pina@gmail.com>
2022-04-04 15:10:39 +0100

    cloudfunctions.invoker

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -11,7 +11,7 @@ resource "google_cloudfunctions_function_iam_binding" "pusher_invoker_binding" {
   project        = google_cloudfunctions_function.pusher.project
   region         = google_cloudfunctions_function.pusher.region
   cloud_function = google_cloudfunctions_function.pusher.name
-  role           = "roles/invoker"
+  role           = "roles/cloudfunctions.invoker"
   members = [
     "allUsers",
   ]
