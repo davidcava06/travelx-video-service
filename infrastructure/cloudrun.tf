d7200c0db366dcbd44d0d670c03195c32217f675
davidcava06 <35784410+davidcava06@users.noreply.github.com>
2022-04-10 11:56:51 +0100

    Update cloudrun.tf

--- infrastructure/cloudrun.tf
+++ infrastructure/cloudrun.tf
@@ -20,8 +20,6 @@ resource "google_cloud_run_service" "api" {
           name  = "ENVIRONMENT"
           value = var.workspace
         }
-        command = ["gunicorn"]
-        args    = ["--bind", ":8000", "--workers", "1", "--threads", "8", "--timeout", "0", "app:app"]
       }
       timeout_seconds      = "30"
       service_account_name = google_service_account.tiktok_api.email
