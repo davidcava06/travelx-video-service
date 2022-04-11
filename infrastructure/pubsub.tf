3756675d08ad0a3a2744ef95abd2ac6b3651ebce
davidcava <david.cava.pina@gmail.com>
2022-04-11 08:37:08 +0100

    update minimum retry

--- infrastructure/pubsub.tf
+++ infrastructure/pubsub.tf
@@ -36,7 +36,7 @@ resource "google_pubsub_subscription" "tiktok_subscription" {
 
   dead_letter_policy {
     dead_letter_topic     = google_pubsub_topic.dead_letter_topic.id
-    max_delivery_attempts = 1
+    max_delivery_attempts = 5
   }
 
   retry_policy {
