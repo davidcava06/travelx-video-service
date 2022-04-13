cf36b3f3fba9ea0a07088edf6034098e21a4ebfb
davidcava <david.cava.pina@gmail.com>
2022-04-14 00:13:33 +0100

    idk

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -8,14 +8,12 @@ resource "google_cloudfunctions_function_iam_binding" "pusher_invoker_binding" {
   ]
 }
 
-resource "google_cloudfunctions_function_iam_binding" "transcoder_invoker_binding" {
+resource "google_cloudfunctions_function_iam_member" "transcoder_invoker_member" {
   project        = google_cloudfunctions_function.transcoder.project
   region         = google_cloudfunctions_function.transcoder.region
   cloud_function = google_cloudfunctions_function.transcoder.name
   role           = "roles/cloudfunctions.invoker"
-  members = [
-    "allUsers",
-  ]
+  member         = "serviceAccount:${google_service_account.cloud_function_invoker_account.email}"
 }
 
 resource "google_cloudfunctions_function_iam_member" "insta_downloader_invoker_member" {
