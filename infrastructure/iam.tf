bac4b47d9cbab46def4422e351bae4c4b2e84055
davidcava <david.cava.pina@gmail.com>
2022-04-13 10:28:46 +0100

    temp http for transcoder for testing

--- infrastructure/iam.tf
+++ infrastructure/iam.tf
@@ -8,6 +8,16 @@ resource "google_cloudfunctions_function_iam_binding" "pusher_invoker_binding" {
   ]
 }
 
+resource "google_cloudfunctions_function_iam_binding" "transcoder_invoker_binding" {
+  project        = google_cloudfunctions_function.transcoder.project
+  region         = google_cloudfunctions_function.transcoder.region
+  cloud_function = google_cloudfunctions_function.transcoder.name
+  role           = "roles/cloudfunctions.invoker"
+  members = [
+    "allUsers",
+  ]
+}
+
 resource "google_cloudfunctions_function_iam_member" "insta_downloader_invoker_member" {
   project        = google_cloudfunctions_function.insta_downloader.project
   region         = google_cloudfunctions_function.insta_downloader.region
