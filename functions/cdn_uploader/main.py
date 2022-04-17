3c39d9bab40bf54076f85c90b692c1e6eac539e4
davidcava <david.cava.pina@gmail.com>
2022-04-17 18:20:25 +0100

    fix slack messaging

--- functions/cdn_uploader/main.py
+++ functions/cdn_uploader/main.py
@@ -164,7 +164,7 @@ def cdn_uploader(event, context):
         )
 
         title = f"{video_path} Here"
-        title_link = cdn_client.base_url + manifest_pair[1] + "?filename=manifest.m3u8"
+        title_link = cdn_client.base_url + manifest_pair[0][1] + "?filename=manifest.m3u8"
         status = Status.success
         msg = f"🎉 Successfully uploaded video to {manifest_cdn_url}"
     except Exception as e:
