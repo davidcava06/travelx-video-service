162ee0c9ae0c031cf9081422aa027af91bb4bd56
fiebel-cicd <runner@fv-az121-983.nibn5qssr4lefd4ngbqlcey4hh.cx.internal.cloudapp.net>
2022-04-18 23:08:10 +0000

    :robot: auto bump cloudrun apps version tag in nonprod

--- infrastructure/cloudrun.tf
+++ infrastructure/cloudrun.tf
@@ -5,7 +5,7 @@ resource "google_cloud_run_service" "api" {
   template {
     spec {
       containers {
-        image = "europe-west2-docker.pkg.dev/${google_artifact_registry_repository.app_registry.project}/${google_artifact_registry_repository.app_registry.repository_id}/tiktok-api:2022-04-15_84_8ec49c3"
+        image = "europe-west2-docker.pkg.dev/${google_artifact_registry_repository.app_registry.project}/${google_artifact_registry_repository.app_registry.repository_id}/tiktok-api:2022-04-18_88_b6153a1"
         resources {
           limits = {
             "cpu"    = "1000m"  //explicit
